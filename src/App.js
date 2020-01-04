591cb2b136990dc4fd6a87830c7461eda96dd75a
Carlos Castro Brenes <ccasbre27@hotmail.com>
2020-01-03 22:05:37 -0600

    added form component to change the new category

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Header from './components/Header';
 import ListNews from './components/ListNews';
+import Form from './components/Form';
 
 class App extends Component {
   state = {
@@ -11,8 +12,8 @@ class App extends Component {
     this.getNews();
   }
 
-  getNews = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=YOUR_API_KEY`;
+  getNews = async (category = 'general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=de&category=${category}&apiKey=YOUR_API_KEY`;
 
     const response = await fetch(url);
     const news = await response.json();
@@ -30,6 +31,8 @@ class App extends Component {
           title="News from Germany" />
 
         <div className="container white contenedor-noticias">
+          <Form 
+            getNews={this.getNews}/>
           <ListNews
             news={this.state.news} />
         </div>
---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+    state = {
+        category: 'general'
+    }
+
+    handleChangeCategory = (e) => {
+        this.setState({
+            // se actualiza la categoria con el valor seleccionado del select option
+            category: e.target.value
+        }, () => {
+            // se hace con un callback porque hay un issue 
+            // pasarlo a la página principal para que actualice las noticias
+            this.props.getNews(this.state.category);
+        })
+
+    }
+
+    render() {
+        return (
+            <div className="buscador row">
+                <div className="col s12 m8 offset-m2">
+                    <form>
+                        <h2>Find news by category</h2>
+                        <div className="input-field col s12">
+                            <select
+                                onChange={this.handleChangeCategory}>
+                                <option value="general">General</option>
+                                <option value="science">Science</option>
+                                <option value="technology">Technology</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Form;
