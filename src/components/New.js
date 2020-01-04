1722fb68fb224b78b378618a2c045c722c075d3b
Carlos Castro Brenes <ccasbre27@hotmail.com>
2020-01-03 21:46:02 -0600

    added design of the new component

--- src/components/New.js
+++ src/components/New.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const New = ({info}) => {
+
+    // extraemos los datos
+    const { urlToImage, url, title, description, source } = info;
+
     return (
-        <h1>Desde noticia</h1>
+        <div className="col s12 m6 l4">
+            <div className="card">
+                <div className="card-image">
+                    <img src={urlToImage} alt={title} />
+                </div>
+
+                <div className="card-content">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+
+                <div className="card-action">
+                    <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-light btn"> Read more </a>
+                </div>
+            </div>
+        </div>
     );
 }
 
